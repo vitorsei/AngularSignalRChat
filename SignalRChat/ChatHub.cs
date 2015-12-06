a9a070c146adb329deeb3c2cccc8abddee7106af
vitorsei <seiji.vitor@gmail.com>
2015-12-06 13:29:07 -0200

    Initial Commit

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRChat
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void SaySomething(string message)
+        {
+            Clients.All.speak(message + "from the server");
+        }
+    }
+}
---
+++ SignalRChat/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
