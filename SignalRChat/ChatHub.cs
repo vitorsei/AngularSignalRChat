de4f91fcf27daebf120eb0803de0be65ff484fd8
vitorsei <seiji.vitor@gmail.com>
2015-12-06 22:25:22 -0200

    Using SignalR Groups & Context with Angular

--- SignalRChat/ChatHub.cs
+++ SignalRChat/ChatHub.cs
@@ -6,9 +6,26 @@ namespace SignalRChat
     [HubName("chat")]
     public class ChatHub : Hub
     {
-        public void SendMessage(string message)
+        public void SendMessage(SendData data)
         {
-            Clients.All.newMessage(message);
+            Clients.Group(data.roomName).newMessage(data.name + ": " + data.message);
         }
+
+        public void JoinRoom(string roomName)
+        {
+            Groups.Add(Context.ConnectionId, roomName);
+        }
+
+        public void LeaveRoom(string roomName)
+        {
+            Groups.Remove(Context.ConnectionId, roomName);
+        }
+    }
+
+    public class SendData
+    {
+        public string roomName { get; set; }
+        public string message { get; set; }
+        public string name { get; set; }
     }
 }
--- SignalRChat/app/mainCtrl.js
+++ SignalRChat/app/mainCtrl.js
@@ -1,9 +1,26 @@
 ﻿angular.module('app').controller('mainCtrl', function ($scope, chat) {
     $scope.messages = [];
 
+    $scope.inRoom = false;
+    $scope.nameSet = false;
+
+    $scope.setName = function () {
+        $scope.nameSet = true;
+    }
+
+    $scope.joinRoom = function () {
+        $scope.inRoom = true;
+        chat.server.joinRoom($scope.roomName);
+    }
+
+    $scope.leaveRoom = function () {
+        $scope.inRoom = false;
+        chat.server.leaveRoom($scope.roomName);
+    }
+
     $scope.sendMessage = function () {
-        chat.server.sendMessage($scope.newMessage);
-        $scope.newMessage = '';
+        chat.server.sendMessage({ name: $scope.name, message: $scope.newMessage, roomName: $scope.roomName });
+        $scope.newMessage = "";
     };
 
     chat.client.newMessage = function onNewMessage(message) {
